1ee9766f5c2c98512cf6bcbd9ef3fce722967258
Nguyen Truong <truongvinhnhunguyen@gmail.com>
2017-05-16 18:22:25 +0700

    getState() works at basic expectation

--- TimerSet.cpp
+++ TimerSet.cpp
@@ -1,4 +1,5 @@
 #include "TimerSet.h"
+#define DEBUG_TIME_SET
 
 TimerSet::TimerSet(UDP& udp, const char* poolServerName){
 	m_timeClient = new NTPClient (udp, poolServerName);
@@ -11,6 +12,7 @@ TimerSet::~TimerSet(){
 void TimerSet::begin(){
 	m_timeClient->begin();
 	m_timeClient->update();
+	m_lastStateChange = m_timeClient->getEpochTime() % 86400;
 }
 
 void TimerSet::end(){
@@ -25,4 +27,80 @@ void TimerSet::printTime(){
 	Serial.println(m_timeClient->getHours());
 	Serial.println(m_timeClient->getMinutes());
 	Serial.println(m_timeClient->getSeconds());
+}
+
+// ============================================================
+// Return 1: ON; 0: OFF; -1: NO EVENT
+// If current state is OFF, check if it should be turned to ON
+// If current state is ON,  check if it should be turned to OFF
+// ============================================================
+
+int TimerSet::getState(){
+	
+	int t;
+	int newState;
+	unsigned int *p;
+	
+	unsigned long secInDay = m_timeClient->getEpochTime() % 86400; 
+	unsigned int sinceLastCheck = secInDay - m_lastCheck;
+	
+	
+	if(sinceLastCheck > 5){ // Check every 5" since last check
+	
+		m_lastCheck = secInDay;
+		
+		if(m_currentState == 0) { // If it is OFF, find event to turn it ON
+			newState = 1;
+			p = m_startTimer;
+			#ifdef DEBUG_TIME_SET
+				Serial.println("OFF finds ON");
+			#endif
+		
+		} else { // If it is ON, find event to turn it OFF
+			newState = 0;
+			p = m_stopTimer;
+			#ifdef DEBUG_TIME_SET
+				Serial.println("ON finds OFF");
+			#endif
+		}	
+			
+		for(int i = 0; i < NUM_TIMERS; i++){
+			if(i == 0)
+				t = secInDay - m_lastStateChange - p[i];
+			else
+				t = secInDay - p[i];
+			
+			#ifdef DEBUG_TIME_SET
+				Serial.print(i);
+				Serial.print(" / ");
+				Serial.print(secInDay);
+				Serial.print(" / ");
+				Serial.print(m_lastStateChange);
+				Serial.print(" / ");
+				Serial.print(p[i]);
+				Serial.print(" / ");
+				Serial.println(t);
+			#endif
+			
+			if (t>=0 && t<5) { // If interval reached
+				m_currentState = newState;
+				m_lastStateChange = secInDay;
+				
+				#ifdef DEBUG_TIME_SET
+					Serial.print("Timer[");
+					Serial.print(i);
+					Serial.print("] CHANGED to: ");
+					Serial.print(m_currentState);
+					Serial.print(" at: ");
+					Serial.println(m_timeClient->getFormattedTime());
+				#endif
+				break;
+			}
+		}
+	} else {
+		return -1;
+	}
+	
+	return m_currentState;
+
 }
--- TimerSet.h
+++ TimerSet.h
@@ -4,13 +4,24 @@
 #ifndef _TIMERSET_
 #define _TIMERSET_
 
+#define NUM_TIMERS 4
 
 class TimerSet{
 
 NTPClient *m_timeClient;
 int m_timeZone = 7;
 
-	
+unsigned int m_lastCheck = 0;
+
+int m_currentState = 0; // 1: ON; 0: OFF;
+unsigned int m_lastStateChange = 0;
+
+// First item is used as Interval
+// From 2nd item moving forward, value is stored as "Second in the day", 
+// Ex, 15:20 = 15*60*60+20*60 = 55,200
+unsigned int m_startTimer[NUM_TIMERS] = {86401, (11*60+17)*60, 86401, 86401}; 
+unsigned int m_stopTimer[NUM_TIMERS] = {86401, (11*60+18)*60, 86401, 86401};
+
 public:
 	TimerSet(UDP& udp, const char* poolServerName);
 	~TimerSet();
@@ -19,6 +30,16 @@ public:
 	
 	void begin();
 	void end();
+	
+	void setTurnOnInterval(int interval);
+	void setTurnOffInterval(int interval);
+	
+	void setTimer(int idx, int onTimer, int duration);
+	
+	int getState();
+	int setState(int state);
+	
+	
 	void printTime();
 };
 
